7d73cfe6c8976bf895390c9f788d950917fcb986
tharitthaveekittikul <tharit.thaveekittikul@gmail.com>
2025-12-17 16:26:02 +0700

    Initial Commit

---
+++ WhatNow/WhatNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatNowApp.swift
+//  WhatNow
+//
+//  Created by Tharit Thaveekittikul on 12/17/25.
+//
+
+import SwiftUI
+
+@main
+struct WhatNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
